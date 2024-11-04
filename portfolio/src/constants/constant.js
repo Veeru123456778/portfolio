0eeb4aabb928295226249a43f7fc6ae444d2ff9d
Varun Kumar <kumarvarunn656@gmail.com>
2024-11-04 13:38:03 +0530

    making config changes

--- portfolio/src/constants/constant.js
+++ portfolio/src/constants/constant.js
@@ -1,6 +1,3 @@
-import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-
 import reactIcon from "../assets/react-icon.svg";
 import bootstrapIcon from "../assets/bootstrap-icon.svg";
 import canvaIcon from "../assets/canva-icon.svg";
--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -6,15 +6,26 @@
 //   plugins: [react()],
 // })
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+
+//     },
+//   },
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Additional Vite configurations
-  resolve: {
-    alias: {
-      // Add any path aliases here if needed
-    },
+  base: './', 
+  build: {
+    outDir: 'dist', 
   },
 });
